53186f47508eb50408d7c637bb1168954b5a3cec
movinoary <97898853+movinoary@users.noreply.github.com>
2025-03-05 23:52:43 +0700

    Update to typescript

--- hari.js
+++ hari.js
@@ -1,5 +1,5 @@
-const startDate = new Date("2025-02-20")
-const endDate = new Date("2025-02-25")
+const startDate: Date = new Date("2025-02-20")
+const endDate: Date = new Date("2025-02-25")
 
-let betweenDate = endDate - startDate
-betweenDate = betweenDate / (1000 * 3600 * 24) // hasil : 5
+let betweenDate: number = endDate.getTime() - startDate.getTime()
+betweenDate = betweenDate / (1000 * 3600 * 24) // hasil: 5
