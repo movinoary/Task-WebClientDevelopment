5fb557a0d0ab4224560a3fb2691b8186ccbfea6b
movinoary <97898853+movinoary@users.noreply.github.com>
2025-03-05 23:09:43 +0700

    Create hari.js

---
+++ hari.js
@@ -0,0 +1,5 @@
+const startDate = new Date("2025-02-20")
+const endDate = new Date("2025-02-25")
+
+let betweenDate = endDate - startDate
+betweenDate = betweenDate / (1000 * 3600 * 24) // hasil : 5
