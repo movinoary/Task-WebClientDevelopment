c7ddaf8e9bd48ec69b6b89b962c444de16fd4d71
movinoary <97898853+movinoary@users.noreply.github.com>
2025-03-05 23:08:58 +0700

    Create lingkaran.js

---
+++ lingkaran.js
@@ -0,0 +1,5 @@
+const radius = 5
+
+const diameter = radius * 2 // hasil : 10
+let circumference = 2 * 22/7 * radius // hasil : 31.428571428571427
+let area = 22/7 * radius * radius // hasil : 78.57142857142857
