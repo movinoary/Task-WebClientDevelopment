b55b9f980b51bd7a9b21c97105b0ad982a08afd7
movinoary <97898853+movinoary@users.noreply.github.com>
2025-03-05 23:52:11 +0700

    Update to typescript

--- lingkaran.js
+++ lingkaran.js
@@ -1,5 +1,5 @@
-const radius = 5
+const radius: number = 5
 
-const diameter = radius * 2 // hasil : 10
-let circumference = 2 * 22/7 * radius // hasil : 31.428571428571427
-let area = 22/7 * radius * radius // hasil : 78.57142857142857
+const diameter: number = radius * 2 // hasil: 10
+let circumference: number = 2 * (22 / 7) * radius // hasil: 31.428571428571427
+let area: number = (22 / 7) * radius * radius // hasil: 78.57142857142857
